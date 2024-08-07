f3c0cb930deb68904d6f8c838b2b5226b39b3f18
ClemenF <clemen.fayol@gmail.com>
2024-08-07 20:06:23 +0200

    v3.1

---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-col
+  class="mx-auto">
+    <v-card>
+      <v-card-item>
+        <v-card-title
+          class="text-center">
+          Mode aléatoire
+        </v-card-title>
+
+        <v-card-text>
+          Le mot sera demandé aléatoirement en anglais ou en français.
+        </v-card-text>
+      </v-card-item>
+    </v-card>
+    <v-card>
+      <v-card-item>
+        <v-card-title
+          class="text-center">
+          Mode français
+        </v-card-title>
+
+        <v-card-text>
+          Le mot sera demandé en français.
+        </v-card-text>
+      </v-card-item>
+    </v-card>
+    <v-card>
+      <v-card-item>
+        <v-card-title
+          class="text-center">
+          Mode Anglais
+        </v-card-title>
+
+        <v-card-text>
+          Le mot sera demandé en anglais.
+        </v-card-text>
+      </v-card-item>
+    </v-card>
+  </v-col>
+</template>
+
+<script setup>
+</script>
+
+<script>
+export default {
+  data () {
+    return {
+      test: 'test'
+    }
+  }
+}
+</script>
